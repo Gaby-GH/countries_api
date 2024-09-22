9f755501a8277d1e702617231fff4ebcae51f564
Gaby-GH <161126492+Gaby-GH@users.noreply.github.com>
2024-09-22 11:09:50 +0200

    Add files via upload

--- pays.js
+++ pays.js
@@ -1,5 +1,5 @@
 // Init
-
+/*
 let DataBase = localStorage.getItem("InfosPays")
 DataBase = JSON.parse(DataBase)
 
@@ -59,15 +59,77 @@ if (Array.isArray(borders)){
         div_border.append(btn)
     }
 }
+*/
+/*
+async function DisplayFrance(pays){
 
-
-async function DisplayFrance(){
-
-    let file = await fetch("https://restcountries.com/v3.1/name/France") // resoudre bug net::ERR_INTERNET_DISCONNECTED
-    let france  = await file.json()                                      // ET finir avec les borders
+    let file = await fetch("https://restcountries.com/v3.1/name/France") 
+    let france  = await file.json()                                      
                                                                         // + faire un systeme de filtre et recherche
     console.log(france)
 }
 
-DisplayFrance()
+DisplayFrance()*/
+
+
+let NameCountry = localStorage.getItem("country")
+
+async function GetCountry(pays){
+
+    let file = await fetch(`https://restcountries.com/v3.1/name/${pays}`)
+    let DataPays = await file.json()
+    
+    console.log(DataPays)
+
+    DisplayCountry(DataPays[0])
+}
+
+async function DisplayCountry(data){
+
+    // Title page
+    let title = document.querySelector("title")
+    title.innerHTML = data.name.common
+
+    // Flag
+    let flag = document.querySelector("#flag")
+    flag.src = data.flags.svg
+
+    // Name
+    let title_pays = document.querySelector("#p_country_name")
+    title_pays.textContent = data.name.common
+
+    // Infos
+
+    let native = document.querySelector("#native")
+    let n = Object.keys(data.name.nativeName)
+    native.textContent = Object.values(data.name.nativeName)[0].common
+
+
+    let population = document.querySelector("#population")
+    population.textContent = data.population
+
+    let region = document.querySelector("#region")
+    region.textContent = data.region
+
+    let sub_region = document.querySelector("#sub_region")
+    sub_region.textContent = data.subregion
+
+    let capital = document.querySelector("#capital")
+    capital.textContent = data.capital[0]
+
+    let domain = document.querySelector("#domain")
+    domain.textContent = data.tld[0]
+
+    let currencies = document.querySelector("#currencies")
+    currencies.textContent = Object.values(data.currencies)[0].name
+
+    let language = document.querySelector("#language")
+    language.textContent = Object.values(data.languages)[0]
+
+
+    // Borders Country
+
+}
+
+GetCountry(NameCountry)
 
